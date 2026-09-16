a4502f73009567b6286601f6853fb44d9909c510
Brad Ben Joya <bradbenjoya6@gmail.com>
2026-09-16 14:45:41 +0300

    Finished initial prototype of the corruptor

--- src/core/Corruptor.cpp
+++ src/core/Corruptor.cpp
@@ -1,9 +1,32 @@
 #include "Corruptor.h"
+#include "Randomizer.h"
+
+#include <cctype>
 #include <fstream>
+#include <stdexcept>
 
 namespace core {
-    void Corruptor::corrupt() {
-        std::ofstream out(m_path += ".corrupted");
-        out.write("i cowuppted the fwile mu heheheheheh", 29);
+void Corruptor::corrupt() {
+    const std::filesystem::path output_path = m_path.string() + ".corrupted";
+
+    std::ifstream input(m_path, std::ios::binary);
+    if (!input.is_open()) {
+        throw std::runtime_error("File not found: " + m_path.string());
     }
+
+    std::ofstream output(output_path, std::ios::binary);
+    if (!output.is_open()) {
+        throw std::runtime_error("Failed to create output file: " + output_path.string());
+    }
+
+    char c = 0;
+    while (input.get(c)) {
+        if (std::isalnum(static_cast<unsigned char>(c))) {
+            const std::size_t random_byte = Randomizer{}.getRandomNumber(0, 255);
+            output.put(static_cast<char>(random_byte));
+        } else {
+            output.put(c);
+        }
+    }
+}
 }
---
+++ src/core/Randomizer.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <random>
+
+class Randomizer {
+    public:
+        Randomizer() = default;
+        ~Randomizer() = default;
+
+        std::size_t getRandomNumber(std::size_t min, std::size_t max) {
+            std::uniform_int_distribution<std::size_t> dist(min, max);
+            return dist(m_gen);
+        }
+
+    private:
+        std::random_device m_rd;
+        std::mt19937 m_gen{m_rd()};
+};
