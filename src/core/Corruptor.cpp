609e2d1395eccf3ca8eb00253389eb918c72232c
Brad Ben Joya <bradbenjoya6@gmail.com>
2026-09-16 14:07:23 +0300

    Corrupter now creates a file with a corrupted prefix so it wont overide the exsisting file.

--- src/core/Corruptor.cpp
+++ src/core/Corruptor.cpp
@@ -1,7 +1,9 @@
 #include "Corruptor.h"
+#include <fstream>
 
 namespace core {
     void Corruptor::corrupt() {
-        
+        std::ofstream out(m_path += ".corrupted");
+        out.write("i cowuppted the fwile mu heheheheheh", 29);
     }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,14 @@
 #include <print>
+#include "core/Corruptor.h"
 
-int main() {
-    std::println("Hello, World!");
+int main(int argc, char** argv) {
+    if (argc != 2) {
+        std::println("Usage: {} <file_to_corrupt>", argv[0]);
+        return 1;
+    }
+    
+    core::Corruptor corruptor(argv[1]);
+    corruptor.corrupt();
+
     return 0;
 }
